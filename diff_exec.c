4a1d49f3e34c05f28a3a93f5e02219394647bdd2
stephaniekioks <kiokosteff.ks@gmail.com>
2023-07-26 02:04:33 -0700

    diff_exec.c

---
+++ diff_exec.c
@@ -0,0 +1,32 @@
+#include "main.h"
+/**
+ * diff_exec - handle or operator
+ * @str: input
+ * Return: nothing
+ */
+void diff_exec(char *str)
+{
+	char *comm;
+	char *end;
+	
+	comm = my_strtok(str, "||");
+
+	while (comm != NULL)
+	{
+		char *single_command = comm;
+
+		while (*single_command == '')
+		{
+			single_comman++;
+		}
+		end = single_command + strlen(single_command) - 1;
+		while (*end == '' &&end >= single_command)
+		{
+			*end = '\0';
+			end--;
+		}
+		execute_command(comm);
+
+		comm = my_strtok(NULL, "||");
+	}
+}
